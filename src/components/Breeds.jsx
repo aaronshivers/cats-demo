d02d98b4b565b3603e0a669fc2ba0ef525dafd3d
as082003 <c_shivers_aaron@cerner.com>
2022-03-27 14:26:32 -0500

    cleanup

--- src/components/Breeds.jsx
+++ src/components/Breeds.jsx
@@ -13,57 +13,53 @@ function Breeds() {
 
   const { data: breeds } = useSWR(`${url}?page=${pageIndex}`, fetcher);
 
-  const renderTable = () => {
-    if (!breeds?.data) {
-      return <Loading />;
-    }
+  if (!breeds?.data) {
+    return <Loading />;
+  }
 
-    return (
-      <>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              {
-                breeds?.data ? (
-                  Object.keys(breeds?.data[0]).map((key) => (
-                    <TableCell scope="col" border="bottom" key={key}>
-                      {key.charAt(0)
-                        .toUpperCase() + key.slice(1)}
-                    </TableCell>
-                  ))
-                ) : null
-              }
-            </TableRow>
-          </TableHeader>
-          <TableBody>
+  return (
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
             {
-              breeds?.data.map(({
-                breed, country, origin, coat, pattern,
-              }) => (
-                <TableRow key={breed}>
-                  <TableCell scope="row"><strong>{breed}</strong></TableCell>
-                  <TableCell>{country}</TableCell>
-                  <TableCell>{origin}</TableCell>
-                  <TableCell>{coat}</TableCell>
-                  <TableCell>{pattern}</TableCell>
-                </TableRow>
-              ))
+              breeds?.data ? (
+                Object.keys(breeds?.data[0]).map((key) => (
+                  <TableCell scope="col" border="bottom" key={key}>
+                    {key.charAt(0)
+                      .toUpperCase() + key.slice(1)}
+                  </TableCell>
+                ))
+              ) : null
             }
-          </TableBody>
-        </Table>
-        <Pagination
-          alignSelf="end"
-          margin="medium"
-          numberItems={breeds.total}
-          onChange={({ page }) => setPageIndex(page)}
-          page={pageIndex}
-          step={breeds.per_page}
-        />
-      </>
-    );
-  };
-
-  return renderTable();
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {
+            breeds?.data.map(({
+              breed, country, origin, coat, pattern,
+            }) => (
+              <TableRow key={breed}>
+                <TableCell scope="row"><strong>{breed}</strong></TableCell>
+                <TableCell>{country}</TableCell>
+                <TableCell>{origin}</TableCell>
+                <TableCell>{coat}</TableCell>
+                <TableCell>{pattern}</TableCell>
+              </TableRow>
+            ))
+          }
+        </TableBody>
+      </Table>
+      <Pagination
+        alignSelf="end"
+        margin="medium"
+        numberItems={breeds.total}
+        onChange={({ page }) => setPageIndex(page)}
+        page={pageIndex}
+        step={breeds.per_page}
+      />
+    </>
+  );
 }
 
 export default Breeds;
