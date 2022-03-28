b0fbfa49c54cd8a6fe5e97dfdc4f77a056d96b76
as082003 <c_shivers_aaron@cerner.com>
2022-03-28 08:59:17 -0500

    cleanup

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,20 +1,21 @@
-/* eslint-disable react/function-component-definition */
 import React from 'react';
 import { Box, Spinner } from 'grommet';
 
-const Loading = () => (
-  <Box
-    align="center"
-    justify="center"
-    direction="row"
-    gap="small"
-    pad="small"
-  >
-    <Spinner
-      margin="large"
-      size="large"
-    />
-  </Box>
-);
+function Loading() {
+  return (
+    <Box
+      align="center"
+      justify="center"
+      direction="row"
+      gap="small"
+      pad="small"
+    >
+      <Spinner
+        margin="large"
+        size="large"
+      />
+    </Box>
+  );
+}
 
 export default Loading;
